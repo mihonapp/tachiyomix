88e3330788eb23294cb6b3f05ee112d1cf661947
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-01-13 18:34:43 +0500

    getPreferences with id

--- library/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
@@ -1,9 +1,25 @@
 package eu.kanade.tachiyomi.source
 
+import android.content.SharedPreferences
 import androidx.preference.PreferenceScreen
 
 @Suppress("unused")
 interface ConfigurableSource {
+
+    /**
+     * Gets instance of [SharedPreferences] scoped to the specific source.
+     *
+     * @since extensions-lib 1.5
+     */
+    fun getSourcePreferences(): SharedPreferences = throw Exception("Stub!")
+
+    /**
+     * Gets instance of [SharedPreferences] corrisponding to the source id
+     *
+     * @parm id the source id
+     * @since extensions-lib 1.5
+     */
+    fun getPreferences(id: Long): SharedPreferences = throw Exception("Stub!")
 
     fun setupPreferenceScreen(screen: PreferenceScreen)
 

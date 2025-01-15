964223b1c82018bba8576c80108075f3a6629216
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-01-15 19:03:19 +0500

    move preference getting function to `Source` (#8)
    * remove protected
    
    * Update ConfigurableSource.kt
    
    * Update Source.kt

--- library/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
@@ -1,26 +1,8 @@
 package eu.kanade.tachiyomi.source
 
-import android.content.SharedPreferences
 import androidx.preference.PreferenceScreen
 
 @Suppress("unused")
 interface ConfigurableSource {
-
-    /**
-     * Gets instance of [SharedPreferences] scoped to the specific source.
-     *
-     * @since extensions-lib 1.6
-     */
-    protected fun getSourcePreferences(): SharedPreferences = throw Exception("Stub!")
-
-    /**
-     * Gets instance of [SharedPreferences] corrisponding to the source id
-     *
-     * @parm id the source id
-     * @since extensions-lib 1.6
-     */
-    protected fun getSourcePreferences(id: Long): SharedPreferences = throw Exception("Stub!")
-
     fun setupPreferenceScreen(screen: PreferenceScreen)
-
 }

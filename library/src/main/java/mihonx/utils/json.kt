e21ce9c21b2c320e163d6bf31d2bc21ad2a29071
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-01-25 20:56:54 +0600

    Rework Json utils

--- library/src/main/java/mihonx/utils/json.kt
+++ library/src/main/java/mihonx/utils/json.kt
@@ -1,12 +1,16 @@
+@file:Suppress("UNUSED")
+
 package mihonx.utils
 
 import kotlinx.serialization.json.Json
 import okhttp3.Response
 
-inline fun <reified T> Response.parseAs(json: Json = throw RuntimeException("Stub!")): T {
-    throw RuntimeException("Stub!")
+val defaultJson: Json = throw RuntimeException("Stub!")
+
+inline fun <reified T> Response.parseAs(json: Json = defaultJson): T {
+    return body.string().parseAs(json)
 }
 
-inline fun <reified T> String.parseAs(json: Json = throw RuntimeException("Stub!")): T {
-    throw RuntimeException("Stub!")
+inline fun <reified T> String.parseAs(json: Json = defaultJson): T {
+    return json.decodeFromString(this)
 }

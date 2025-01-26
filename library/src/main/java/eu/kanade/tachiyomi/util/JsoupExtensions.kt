1173b6249e86d93233cd48c0a6c55b1c5d1e2910
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-01-26 15:54:56 +0600

    Deprecate Jsoup util function and encourage Ksoup

--- library/src/main/java/eu/kanade/tachiyomi/util/JsoupExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/util/JsoupExtensions.kt
@@ -7,6 +7,13 @@ import org.jsoup.nodes.Document
  * Returns a Jsoup document for this response.
  * @param html the body of the response. Use only if the body was read before calling this method.
  */
+@Deprecated(
+    message = "Use Ksoup instead",
+    replaceWith = ReplaceWith(
+        expression = "parseAsDocument()",
+        imports = ["mihonx.utils.parseAsDocument"]
+    ),
+)
 fun Response.asJsoup(html: String? = null): Document {
     throw RuntimeException("Stub!")
 }

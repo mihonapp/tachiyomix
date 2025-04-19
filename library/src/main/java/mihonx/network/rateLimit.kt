1cc62f22ddc6930e256c8b41dc5e8776b63ae2c2
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-04-19 15:02:04 +0500

    Use `httpUrl.host` for ratelimit (#13)

--- library/src/main/java/mihonx/network/rateLimit.kt
+++ library/src/main/java/mihonx/network/rateLimit.kt
@@ -3,6 +3,7 @@
 package mihonx.network
 
 import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
@@ -30,8 +31,8 @@ inline fun OkHttpClient.Builder.rateLimit(
  *
  * Examples:
  *
- * url = "https://api.manga.example", permits = 5, period = 1.seconds =>  5 requests per second to any url starting with "https://api.manga.example"
- * url = "https://cdn.manga.example/image", permits = 10, period = 2.minutes  =>  10 requests per 2 minutes to any url starting with "https://cdn.manga.example/image"
+ * url = "https://api.manga.example", permits = 5, period = 1.seconds =>  5 requests per second to any url with host == "api.manga.example"
+ * url = "https://cdn.manga.example/image", permits = 10, period = 2.minutes  =>  10 requests per 2 minutes to any url with host == "cdn.manga.example"
  *
  * @since extension-lib 1.6
  *
@@ -43,15 +44,15 @@ inline fun OkHttpClient.Builder.rateLimit(
     url: String,
     permits: Int,
     period: Duration = 1.seconds,
-): OkHttpClient.Builder = rateLimit(permits, period) { it.toString().startsWith(url) }
+): OkHttpClient.Builder = rateLimit(url.toHttpUrl(), permits, period)
 
 /**
  * An OkHttp interceptor that handles given url host's rate limiting.
  *
  * Examples:
  *
- * httpUrl = "https://api.manga.example".toHttpUrlOrNull(), permits = 5, period = 1.seconds =>  5 requests per second to any url starting with "https://api.manga.example"
- * httpUrl = "https://cdn.manga.example/image".toHttpUrlOrNull(), permits = 10, period = 2.minutes  =>  10 requests per 2 minutes to any url starting with "https://cdn.manga.example/image"
+ * httpUrl = "https://api.manga.example".toHttpUrlOrNull(), permits = 5, period = 1.seconds =>  5 requests per second to any url with host == "api.manga.example"
+ * httpUrl = "https://cdn.manga.example/image".toHttpUrlOrNull(), permits = 10, period = 2.minutes  =>  10 requests per 2 minutes to any url with host == "cdn.manga.example
  *
  * @since extension-lib 1.6
  *
@@ -63,7 +64,7 @@ inline fun OkHttpClient.Builder.rateLimit(
     httpUrl: HttpUrl,
     permits: Int,
     period: Duration = 1.seconds,
-): OkHttpClient.Builder = rateLimit(httpUrl.toString(), permits, period)
+): OkHttpClient.Builder = rateLimit(permits, period) { it.host == httpUrl.host }
 
 /**
  * An OkHttp interceptor that enforces conditional rate limiting based on a given condition.

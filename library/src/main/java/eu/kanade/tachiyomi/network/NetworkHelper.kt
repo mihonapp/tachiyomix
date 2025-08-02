aae94b4be4f8714a76bb8d4bd2cf7729f4e152c3
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-08-02 20:24:54 +0600

    Deprecate `NetworkHelper.cloudflareClient`

--- library/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -7,5 +7,9 @@ class NetworkHelper(context: Context) {
 
     val client: OkHttpClient = throw Exception("Stub!")
 
+    @Deprecated(
+        message = "The provided regular client should have cloudflare handling capability",
+        replaceWith = ReplaceWith("client"),
+    )
     val cloudflareClient: OkHttpClient = throw Exception("Stub!")
 }

158d1e64474f23238ce0a2f3313220ab26964e44
paul-arthurthiery <p.thiery14@gmail.com>
2025-08-22 09:40:49 -0400

    Add full chapter download API

--- library/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
@@ -1,10 +1,20 @@
 package eu.kanade.tachiyomi.source
 
+import android.content.SharedPreferences
 import androidx.preference.PreferenceScreen
 
 @Suppress("unused")
 interface ConfigurableSource {
 
     fun setupPreferenceScreen(screen: PreferenceScreen)
+
+    /**
+     * Gets instance of [SharedPreferences] scoped to the specific source.
+     * This is used to access source-specific preferences, including full chapter download settings.
+     *
+     * @since extensions-lib 1.5
+     * @return SharedPreferences instance for this source
+     */
+    fun sourcePreferences(): SharedPreferences = throw Exception("Stub!")
 
 }
--- library/src/main/java/eu/kanade/tachiyomi/source/Source.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/Source.kt
@@ -3,7 +3,14 @@ package eu.kanade.tachiyomi.source
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.Response
 import rx.Observable
+
+/**
+ * Preference key for enabling full chapter downloads in extensions.
+ * Extensions should use this key when adding a boolean preference for full chapter downloads.
+ */
+const val FULL_CHAPTER_DOWNLOAD_PREF_KEY = "full_chapter_download_enabled"
 
 /**
  * A basic interface for creating a source. It could be an online source, a local source, etc...
@@ -59,4 +66,39 @@ interface Source {
         ReplaceWith("getChapterList"),
     )
     fun fetchChapterList(manga: SManga): Observable<List<SChapter>> = throw Exception("Stub!")
+}
+
+/**
+ * A source that supports downloading complete chapters as archive files (e.g., CBZ).
+ * This is an optional interface that sources can implement to provide more efficient
+ * chapter downloads when the source hosts complete chapter files.
+ *
+ * @since extensions-lib 1.6
+ */
+interface FullChapterSource : Source {
+
+    /**
+     * Indicates whether this source supports downloading complete chapters as archive files.
+     * This should return true only if the source can provide complete chapter files
+     * (e.g., CBZ, ZIP) instead of individual page images.
+     *
+     * @since extensions-lib 1.6
+     * @return true if the source supports full chapter downloads, false otherwise
+     */
+    fun supportsFullChapterDownload(): Boolean = false
+
+    /**
+     * Downloads a complete chapter as an archive file (e.g., CBZ).
+     * This method is only called if [supportsFullChapterDownload] returns true
+     * and the user has enabled full chapter downloads in the source's preferences.
+     *
+     * The returned Response should contain the complete chapter archive file.
+     * The Content-Type should be appropriate for the archive format (e.g., application/zip).
+     *
+     * @since extensions-lib 1.6
+     * @param chapter the chapter to download as a complete archive
+     * @return Response containing the complete chapter archive file
+     * @throws Exception if the chapter cannot be downloaded as a complete archive
+     */
+    suspend fun getFullChapter(chapter: SChapter): Response = throw Exception("Stub!")
 }

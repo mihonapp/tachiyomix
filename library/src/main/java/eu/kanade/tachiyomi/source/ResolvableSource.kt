032371e113026765b5c1b1480340cc74c6dd2a53
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-08-30 14:58:59 +0600

    Some review changes and KDoc

--- library/src/main/java/eu/kanade/tachiyomi/source/ResolvableSource.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/ResolvableSource.kt
@@ -2,18 +2,41 @@ package eu.kanade.tachiyomi.source
 
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.HttpUrl
 
-// TODO(antsy): add KDoc
+/**
+ * Represents a source that can resolve a URL into a specific type of [Resource].
+ */
 interface ResolvableSource {
 
-    fun getType(uri: String): UriType?
+    /**
+     * Checks whether the given [url] can be resolved by this source.
+     *
+     * @param url The [HttpUrl] to test.
+     * @return `true` if this source can resolve the URL, `false` otherwise.
+     */
+    fun canResolve(url: HttpUrl): Boolean
 
-    suspend fun getManga(uri: String): SManga?
+    /**
+     * Resolves the given [url] into a [Resource].
+     *
+     * @param url The [HttpUrl] to resolve.
+     * @return The resolved [Resource].
+     * @throws Exception if the resource cannot be resolved.
+     */
+    suspend fun getResource(url: HttpUrl): Resource
 
-    suspend fun getChapter(uri: String): Pair<SManga, SChapter?>?
+    sealed interface Resource {
 
-    sealed interface UriType {
-        data object Manga : UriType
-        data object Chapter : UriType
+        /**
+         * A manga resource, optionally containing a specific chapter.
+         *
+         * @property manga The resolved manga metadata.
+         * @property chapter An optional specific chapter of the manga.
+         */
+        data class Manga(
+            val manga: SManga,
+            val chapter: SChapter? = null
+        ) : Resource
     }
 }

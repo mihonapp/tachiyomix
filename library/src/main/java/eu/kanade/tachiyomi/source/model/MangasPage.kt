fb57cf37422aedd59842e059f8f3b1149dcc3c0d
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-04-25 21:34:25 +0600

    Make MangasPage a regular class

--- library/src/main/java/eu/kanade/tachiyomi/source/model/MangasPage.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/model/MangasPage.kt
@@ -1,3 +1,14 @@
 package eu.kanade.tachiyomi.source.model
 
-data class MangasPage(val mangas: List<SManga>, val hasNextPage: Boolean)
+@Suppress("Unused")
+class MangasPage(val mangas: List<SManga>, val hasNextPage: Boolean) {
+
+    @Deprecated("MangasPage is now a regular class")
+    operator fun component1(): List<SManga> = throw Exception("Stub!")
+
+    @Deprecated("MangasPage is now a regular class")
+    operator fun component2(): Boolean = throw Exception("Stub!")
+
+    @Deprecated("MangasPage is now a regular class")
+    fun copy(mangas: List<SManga> = this.mangas, hasNextPage: Boolean = this.hasNextPage): MangasPage = throw Exception("Stub!")
+}
